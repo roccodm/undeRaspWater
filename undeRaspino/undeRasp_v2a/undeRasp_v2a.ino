5f2b878233be47f2ad715e8aa694da16418fecb4
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2018-02-08 16:05:44 +0100

    Use single timer (timer1) set to 1hz

--- undeRaspino/undeRasp_v2a/undeRasp_v2a.ino
+++ undeRaspino/undeRasp_v2a/undeRasp_v2a.ino
@@ -142,46 +142,34 @@ void setup(){
 
   cli();//stop interrupts
 
-  //set timer0 interrupt at 2kHz
-  TCCR0A = 0;// set entire TCCR0A register to 0
-  TCCR0B = 0;// same for TCCR0B
-  TCNT0  = 0;//initialize counter value to 0
-  // set compare match register for 2khz increments
-  OCR0A = 124;// = (16*10^6) / (2000*64) - 1 (must be <256)
-  // turn on CTC mode
-  TCCR0A |= (1 << WGM01);
-  // Set CS01 and CS00 bits for 64 prescaler
-  TCCR0B |= (1 << CS01) | (1 << CS00);   
-  // enable timer compare interrupt
-  TIMSK0 |= (1 << OCIE0A);
-
-  //set timer1 interrupt at 1Hz
-  TCCR1A = 0;// set entire TCCR1A register to 0
-  TCCR1B = 0;// same for TCCR1B
-  TCNT1  = 0;//initialize counter value to 0
-  // set compare match register for 1hz increments
-  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  // TIMER 1 for interrupt frequency 1 Hz:
+  TCCR1A = 0; // set entire TCCR1A register to 0
+  TCCR1B = 0; // same for TCCR1B
+  TCNT1  = 0; // initialize counter value to 0
+  // set compare match register for 1 Hz increments
+  OCR1A = 62499; // = 16000000 / (256 * 1) - 1 (must be <65536)
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
-  // Set CS10 and CS12 bits for 1024 prescaler
-  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // Set CS12, CS11 and CS10 bits for 256 prescaler
+  TCCR1B |= (1 << CS12) | (0 << CS11) | (0 << CS10);
   // enable timer compare interrupt
   TIMSK1 |= (1 << OCIE1A);
-
-  //set timer2 interrupt at 8kHz
-  TCCR2A = 0;// set entire TCCR2A register to 0
-  TCCR2B = 0;// same for TCCR2B
-  TCNT2  = 0;//initialize counter value to 0
-  // set compare match register for 8khz increments
-  OCR2A = 249;// = (16*10^6) / (8000*8) - 1 (must be <256)
-  // turn on CTC mode
-  TCCR2A |= (1 << WGM21);
-  // Set CS21 bit for 8 prescaler
-  TCCR2B |= (1 << CS21);   
-  // enable timer compare interrupt
-  TIMSK2 |= (1 << OCIE2A);
+  sei(); // allow interrupts
 
   sei();//allow interrupts
 
 }
 
+int dbg_counter = 0;
+void dbg_timer() {
+   char buf[16];
+   itoa(dbg_counter, buf, 10);
+   Serial.println(buf);
+   dbg_counter++;
+}
+
+ISR(TIMER1_COMPA_vect){
+   // Timer1 interrupt
+   dbg_timer(); // used for serial debug, disable when going live
+
+}
