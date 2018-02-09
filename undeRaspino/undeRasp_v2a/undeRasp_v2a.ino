905d650ae36af35e7a8c706798ff2c139adb6673
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2018-02-09 08:03:34 +0100

    Work on rpi start/stop

--- undeRaspino/undeRasp_v2a/undeRasp_v2a.ino
+++ undeRaspino/undeRasp_v2a/undeRasp_v2a.ino
@@ -8,6 +8,9 @@
 RTC_DS1307 RTC;
 DateTime now;
 char buffer[BUFFSIZE];      // general purpose global buffer
+bool first_start = true;    // true when first start, used to run self-checks
+int rpi_cooldown = 0;       // the rpi start/stop cooldown
+bool rpi_started = false;   // set to true when rpi has been started
 bool error_status = false;  // flag setted up in case of fatal errors
 double i2c_val;		    // return value for i2c operations
 
@@ -45,9 +48,46 @@ void i2c_receive(int count) {
 
 void i2c_send() { Wire.write(buffer); }
 
+void start_rpi() {
+#if DEBUG
+	Serial.println("Starting raspberry");
+#endif
+	digitalWrite(RELAY_SET_PIN, 1);
+	delay(100);
+	digitalWrite(RELAY_SET_PIN, 0);
+	rpi_started = true;
+	rpi_cooldown = 0;
+}
+
+void stop_rpi() {
+#if DEBUG
+	Serial.println("Stopping raspberry");
+#endif
+	digitalWrite(RELAY_RESET_PIN, 1);
+	delay(100);
+	digitalWrite(RELAY_RESET_PIN, 0);
+	rpi_started = false;
+	rpi_cooldown = 0;
+}
+
+bool is_rpi_running() { return digitalRead(RASPBERRY_STATUS_PIN) == 1; }
+
 void loop() {
 	if (error_status) {
 		return;  // Disable loop if an error happend
+	}
+	if (first_start) {
+		// do first run, raspberry will do self test and shutdown
+		if (!rpi_started) {
+			start_rpi();
+		} else if (!is_rpi_running() &&
+			   rpi_cooldown > RPI_START_COOLDOWN) {
+			// raspberry has shutdown?! reset first start, entering
+			// normal operation mode
+			first_start = false;
+			stop_rpi();
+		}
+	} else {
 	}
 }
 
@@ -114,6 +154,7 @@ void setup() {
 	sei();  // allow interrupts
 }
 
+#if DEBUG
 int dbg_counter = 0;
 void dbg_timer() {
 	char buf[16];
@@ -121,8 +162,18 @@ void dbg_timer() {
 	Serial.println(buf);
 	dbg_counter++;
 }
+#endif
 
+// Timer1 interrupt
 ISR(TIMER1_COMPA_vect) {
-	// Timer1 interrupt
+#if DEBUG
 	dbg_timer();  // used for serial debug, disable when going live
+#endif
+
+	// rpi start/stop cooldown
+	if (rpi_started && rpi_cooldown < RPI_START_COOLDOWN) {
+		rpi_cooldown += 1;
+	} else if (!rpi_started && rpi_cooldown < RPI_STOP_COOLDOWN) {
+		rpi_cooldown += 1;
+	}
 }
